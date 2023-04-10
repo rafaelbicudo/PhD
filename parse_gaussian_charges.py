9cea32557e7aed3371ab85db0c6e98b84432d2ff
Rafael Bicudo Ribeiro <33603183+rafaelbicudo@users.noreply.github.com>
2023-04-10 16:07:57 -0300

    Add files via upload

--- parse_gaussian_charges.py
+++ parse_gaussian_charges.py
@@ -39,7 +39,7 @@ def find_natoms(topfile: str) -> int:
 
 	return int(words[0])
 
-def check_top_order(topfile: str) -> list[str]:
+def top_order(topfile: str) -> list:
 	"""
 	Return a vector with atoms types ordered by line.
 
@@ -144,16 +144,15 @@ def read_mulliken_charges(topfile: str, gaussianlogfile: str) -> list:
 
 	return qm_charges, gaussian_atoms
 
-def correct_total_charge(qm_charges: list) -> list[float]:
+def correct_total_charge(qm_charges: list) -> list:
 	"""
 	Change the charge of the first atom to ensure quantum charges sum up to zero.
 
 	PARAMETERS:
-	topfile [type: str] -> topology (.itp) file
-	gaussianlogfile [type: str] -> Gaussian09/16 output file with partial charges
+	qm_charges [type: list[float]] -> list with partial charges.
 
 	OUTPUT:
-	qm_charges [type: list[float]] -> List with corrected partial charge summing up to zero
+	qm_charges [type: list[float]] -> list with corrected partial charge summing up to zero.
 	"""
 
 	sum = 0
@@ -169,7 +168,34 @@ def correct_total_charge(qm_charges: list) -> list[float]:
 
 	return qm_charges
 
-def parse_charges(topfile, gaussianlogfile, method):
+def reorder_top_charges(qm_charges: list, grofile: str) -> list:
+	"""
+	Change the ordering of atoms to match with the .gro file.
+
+	PARAMETERS:
+	qm_charges [type: list[float]] -> list with partial charges.
+	grofile [type: str] -> .gro file where atomic ordering matchs the topology file.
+
+	OUTPUT:
+	qm_charges [type: list[float]] -> list with partial charges reordering according to the .gro file.
+	"""
+
+	qm_charges_reordered = [0] * len(qm_charges)
+
+	with open(grofile, "r") as f:
+		for i in range(3):
+			line = f.readline()
+		
+		for j in range(len(qm_charges)):
+			words = line.split()
+			qm_charges_reordered[j] = qm_charges[int(words[2])-1]
+			line = f.readline()
+
+	print("Charges were reordered using {} file.".format(grofile))
+
+	return qm_charges_reordered
+
+def parse_charges(topfile: str, gaussianlogfile: str, method: str, grofile: str):
 	"""
 	Write the new parsed_* file with quantum mechanical charges.
 
@@ -177,6 +203,7 @@ def parse_charges(topfile, gaussianlogfile, method):
 	topfile [type: str] -> topology (.itp) file
 	gaussianlogfile [type: str] -> Gaussian09/16 output file with partial charges
 	method [type: str] -> method used to determine the partial charges
+	grofile [type: str] -> .gro file where atomic ordering matchs the topology file.
 
 	OUTPUT:
 	A new "parsed_*.itp" file with charges updated.
@@ -200,7 +227,7 @@ def parse_charges(topfile, gaussianlogfile, method):
 		fout.write(line)
 		line = f.readline()
 
-	# Use the appropriate function to collect QM charges from each method
+		# Use the appropriate function to collect QM charges from each method
 
 		ESP_methods = ['hly', 'chelpg', 'mk', 'chelp', 'mkuff', 'hlygat']
 		if method.lower() in ESP_methods:
@@ -211,21 +238,34 @@ def parse_charges(topfile, gaussianlogfile, method):
 			print('Charge population method not supported (try "python3 parse_gaussian_charges.py -h").')
 			sys.exit(0)
 
-	# Check if topology and gaussian output atoms are in the same order
+		# Check if topology and gaussian output atoms are in the same order
 
-		topology_atoms = check_top_order(topfile)
+		topology_atoms = top_order(topfile)
+
+		diff_order = False
 
 		for i in range(len(topology_atoms)):
 			if topology_atoms[i] != gaussian_atoms[i]:
-				print('Atoms in the topology file are ordered differently from the gaussian output file. \nAtom {}: {} (topology file) and {} (gaussian file). \nPlease order both files in the same way.'.format(i+1, topology_atoms[i], gaussian_atoms[i]))
-				sys.exit(0)
+				diff_order = True
+				if args.grofile:
+					break
+				else:
+					print("""Atoms in the topology file are ordered differently from the gaussian output file. 
+Atom {}: {} (topology file) and {} (gaussian file).\nPlease order both files in the same way or provide the \
+correctly ordered .gro file using the -g flag.""".format(i+1, topology_atoms[i], gaussian_atoms[i]))
+					sys.exit(0)
 
-	# Correct the total charge
+		# Correct the total charge
 
 		qm_charges = correct_total_charge(qm_charges)
 		i = 0
 
-	# Write the new .itp with QM charges
+		# If required, reorder the atomic charges according to the .gro file matching with .itp file
+
+		if args.grofile:
+			qm_charges = reorder_top_charges(qm_charges, grofile)
+
+		# Write the new .itp with QM charges
 
 		while ("opls" in line) or ("ppg" in line) or line.strip().startswith(";"):
 			if line.strip().startswith(";"):
@@ -234,7 +274,9 @@ def parse_charges(topfile, gaussianlogfile, method):
 				continue
 
 			words = line.split()
-			line = line.replace(words[6], str(qm_charges[i]))
+			# line = line.replace(words[6], str(qm_charges[i]))
+			line = '{:>6}{:>11}{:>7}{:>7}{:>7}{:>7}{:>11.4f}{:>11.4f}\n'.format(words[0], words[1], words[2], words[3],
+					words[4], words[5], qm_charges[i], float(words[7]))
 			fout.write(line)
 			line = f.readline()
 			i = i + 1
@@ -248,10 +290,11 @@ def parse_charges(topfile, gaussianlogfile, method):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Recieves a GROMACS topology generated from LigParGen and Gaussian output file to parse QM partial charges.")
-	parser.add_argument("topfile", help="the topology file (.top file) containing the molecule data for the OPLS-AA force field.")
-	parser.add_argument("gaussianlogfile", help="the gaussian log file with partial charges.")
-	parser.add_argument("--method", "-m", help="the method used to determine the charge populations (chelp, chelpg, mk, mkuff, hly or hlygat).")
+	parser.add_argument("topfile", type=str, help="the topology file (.top file) containing the molecule data for the OPLS-AA force field.")
+	parser.add_argument("gaussianlogfile", type=str, help="the gaussian log file with partial charges.")
+	parser.add_argument("--method", "-m", type=str, help="the method used to determine the charge populations (chelp, chelpg, mk, mkuff, hly or hlygat).")
+	parser.add_argument("--grofile", "-g", type=str, help="the .gro file with the same order as the topology file.")
 
 	args = parser.parse_args()
 
-	parse_charges(args.topfile, args.gaussianlogfile, args.method)
+	parse_charges(args.topfile, args.gaussianlogfile, args.method, args.grofile)
