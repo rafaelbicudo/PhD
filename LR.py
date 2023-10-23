42e3d205e0d318b1df58aa3c0d83760890c78d14
Rafael Bicudo Ribeiro <33603183+rafaelbicudo@users.noreply.github.com>
2023-10-23 13:56:51 -0300

    Added a flag to input the points to be fitted
    Now, instead of performing the linear regression using all the points in the scan files, a list of dihedral angles can be provided as input to perform the regression only using those points. It's important to avoid overfitting.

--- LR.py
+++ LR.py
@@ -14,6 +14,7 @@ that yielded better results.
 [X] Fix the linear fit problems when removing a few angles during the scan due to the atomic nuclei overlapping. 
 	It seems that distances smaller than 0.5 Angstrom crash the code.
 [ ] Add an option to determine the RB coefficients from the total energy instead of the equivalent "QM" torsional energy.
+[ ] Add an option to read a list of angles and fit the coefficients only to these dihedrals.
 
 Author: Rafael Bicudo Ribeiro (@rafaelbicudo) and Thiago Duarte (@thiagodsd)
 DATE: DEZ/2022
@@ -121,7 +122,7 @@ def find_dihedrals(topfile: str, a1: int, a2: int, a3: int, a4: int) -> list:
 
 	return tors, tors_label
 
-def write_torsional_changes(xyzrotationsfile: str, topfile: str, a1: int, a2: int, a3: int, a4: int, npoints: int):
+def get_dihedrals(xyzrotationsfile: str, topfile: str, a1: int, a2: int, a3: int, a4: int, npoints: int, angles: list = []) -> pd.DataFrame:
 	"""
 	Read the .xyz file and find torsional angle changes.
 
@@ -133,11 +134,14 @@ def write_torsional_changes(xyzrotationsfile: str, topfile: str, a1: int, a2: in
 	a3 [type: int] - third atom defining the dihedral angle
 	a4 [type: int] - fourth atom defining the dihedral angle
 	npoints [type: int] - number of configurations during the scan
+	angles [type: np.ndarray] - list with angles to be used in the fit (default is to use all angles)
 
 	OUTPUT:
-	The "dihedrals.csv" file.
+	df_fit [type: pd.DataFrame] - dihedral angles which changed during the scan and will be used for fitting
+	df [type: pd.DataFrame] - dihedral angles which changed during the scan
 	"""
 
+	dih = []
 	atomsCoord = {}
 	i = 0
 
@@ -156,11 +160,16 @@ def write_torsional_changes(xyzrotationsfile: str, topfile: str, a1: int, a2: in
 
 		# Loop over all configurations
 		for conf in range(npoints):
+			while "Dihedral = " not in line:
+				line = xyz_f.readline()
+			
+			dih.append(float(line.split()[2]))
+
 			while len(words) != 4:
 				line = xyz_f.readline()
 				words = line.split()
 
-			# Parse the atomic coordinates to a dictionary (adapted from "parse_txt" function from DICEtools)
+			# Parse the atomic coordinates to a dictionary (adapted from "parse_txt" function of DICEtools)
 			anum = 1
 			for i in range(natoms):
 				atomsCoord[anum] = [float(x) for x in line.split()[1:4]]
@@ -178,10 +187,34 @@ def write_torsional_changes(xyzrotationsfile: str, topfile: str, a1: int, a2: in
 			if (column == column[0]).all():
 				del df[label]
 
-	# Sort the values so the CubicSpline interpolation doesn't complain
-	df = df.sort_values(by=df.columns[0], ascending=True)
+	# Add the dihedral angle to the dataframe
+	dih = np.array(dih)*np.pi/180.
+	df.insert(0, 'Dihedral (rad)', dih)
 
-	df.to_csv('dihedrals.csv')
+	# Sort the df following the dihedral angle
+	df = df.sort_values(by='Dihedral (rad)', ascending=True)
+	df = df.reset_index(drop=True)
+
+	# Select only the angles indicated to be fitted
+	if angles:
+		# Convert the angles to a numpy array and change it to radians
+		angles = np.asarray(angles)
+		angles = angles * np.pi/180
+		
+		# Create a list with indexes to be considered
+		fit = []
+
+		# Find and remove the angles not required
+		for i in range(len(df['Dihedral (rad)'])):
+			for j in angles:
+				if np.abs(df.loc[i, 'Dihedral (rad)'] - j) < 0.08:
+					fit.append(i)
+
+		df_fit = df.loc[df.index.isin(fit)]
+	else:
+		df_fit = df
+
+	return df_fit, df
 
 def check_overlap(topfile: str, xyzrotationsfile: str, cutoff: float, npoints: int) -> list:
 	"""
@@ -246,7 +279,7 @@ def check_overlap(topfile: str, xyzrotationsfile: str, cutoff: float, npoints: i
 
 	return overlap_dih, conf_dih
 
-def remove_overlap(topfile: str, xyzrotationsfile: str, cutoff: float, npoints: int):
+def remove_overlap(topfile: str, xyzrotationsfile: str, cutoff: float, npoints: int) -> None:
 	"""
 	Remove the configurations with atomic overlap.
 
@@ -274,7 +307,7 @@ def remove_overlap(topfile: str, xyzrotationsfile: str, cutoff: float, npoints: 
 	data.to_csv("dihedrals.csv")
 	ans.to_csv("qm_scan.csv")
 
-def write_dih_csv(gaussianlogfile: str, txtfile: str, dfrfile: str, a1: int, a2: int, a3: int, a4: int, maxvalue=None):
+def get_data(gaussianlogfile: str, txtfile: str, dfrfile: str, a1: int, a2: int, a3: int, a4: int) -> pd.DataFrame:
 	"""
 	Read the gaussian output file and write the .csv file with dihedrals and energies.
 
@@ -288,7 +321,8 @@ def write_dih_csv(gaussianlogfile: str, txtfile: str, dfrfile: str, a1: int, a2:
 	a4 [type: int] - fourth atom defining the dihedral angle
 
 	OUTPUT:
-	A "qm_scan.csv" file.
+	scan [type: pd.DataFrame] - data from classical and quantum scan simulations
+	scan_fit [type: pd.DataFrame] - data from classical and quantum scan simulations to be fitted
 	"""
 
 	# Read the dihedrals (in degrees) and energies (in kcal/mol)
@@ -315,28 +349,83 @@ def write_dih_csv(gaussianlogfile: str, txtfile: str, dfrfile: str, a1: int, a2:
 			print("Quantum dihedral - Classical dihedral = ", diff)
 			exit()
 
-	# Subtract the non-bonded energy from lower QM energy configuration
-	nben = [x-nben[np.argmin(enqm)] for x in nben]
-
 	# Convert lists to numpy arrays
 	died = np.asarray(died)
 	enqm = np.asarray(enqm)
 	nben = np.asarray(nben)
 	diedEn = np.asarray(diedEn)
 
+	# Create a pd.DataFrame
+	scan = pd.DataFrame(
+		{
+		'Dihedral (rad)': died,
+		'E_qm (kcal/mol)': enqm,
+		'Non-bonded (kcal/mol)': nben, 
+		}
+	)
+
+	return scan
+
+def rescale_data(scan: pd.DataFrame, angles: list = [], maxvalue=None) -> pd.DataFrame:
+	"""
+	Rescale the data to perform the linear regression.
+
+	PARAMETERS:
+	scan [type: pd.DataFrame] - data from classical and quantum scan simulations
+	angles [type: np.ndarray] - list with angles to be used in the fit (default is to use all angles)
+	maxvalue [type: float] - truncate the energy up to [-maxvalue, maxvalue]
+	"""
+
+	# Select only the angles indicated to be fitted
+	if angles:
+		# Convert the angles to a numpy array and change it to radians
+		angles = np.asarray(angles)
+		angles = angles * np.pi/180
+		
+		# Create a list with indexes to be considered
+		fit = []
+
+		# Find and remove the angles not required
+		for i in range(len(scan['Dihedral (rad)'])):
+			for j in angles:
+				if np.abs(scan.loc[i, 'Dihedral (rad)'] - j) < 0.08:
+					fit.append(i)
+
+		scan_fit = scan.loc[scan.index.isin(fit)]
+
+	else:
+		scan_fit = scan
+
+	# Create a copy to avoid warning messages
+	scan_fit = scan_fit.copy()
+
+	# Subtract the non-bonded energy from lower QM energy configuration
+	enqm_0 = scan_fit.loc[:, 'E_qm (kcal/mol)'].min()
+	nben_0 = scan_fit.loc[scan_fit.loc[:, 'E_qm (kcal/mol)'] == enqm_0, 'Non-bonded (kcal/mol)'].values[0]
+
+	# Subtract the non-bonded energy from both scans
+	scan_fit.loc[:, 'Non-bonded (kcal/mol)'] = scan_fit.loc[:, 'Non-bonded (kcal/mol)'] - nben_0
+	scan.loc[:, 'Non-bonded (kcal/mol)'] = scan.loc[:, 'Non-bonded (kcal/mol)'] - nben_0
+
+	# Rescale the QM energy
+	scan_fit.loc[:, 'E_qm (kcal/mol)'] = scan_fit.loc[:, 'E_qm (kcal/mol)'] - enqm_0
+	scan.loc[:, 'E_qm (kcal/mol)'] = scan.loc[:, 'E_qm (kcal/mol)'] - enqm_0
+
 	# Determine the "QM" torsional energy
-	enfit = enqm - min(enqm) - nben# + diedEn[np.argmin(enqm)]
+	scan_fit.loc[:, 'U_tors-qm (kcal/mol)'] =  scan_fit.loc[:, 'E_qm (kcal/mol)'] - scan_fit.loc[:, 'Non-bonded (kcal/mol)']
+	scan.loc[:, 'U_tors-qm (kcal/mol)'] = scan.loc[:, 'E_qm (kcal/mol)'] - scan.loc[:, 'Non-bonded (kcal/mol)']
+
+	# Rename the columns
+	scan_fit = scan_fit.rename(columns={'Non-bonded (kcal/mol)': 'Non-bonded - nben0 (kcal/mol)', 
+										'E_qm (kcal/mol)': 'E_qm - enqm_0 (kcal/mol)'})
+	scan = scan.rename(columns={'Non-bonded (kcal/mol)': 'Non-bonded - nben0 (kcal/mol)', 
+								'E_qm (kcal/mol)': 'E_qm - enqm_0 (kcal/mol)'})
 
 	# Check for energy barrier limit request
 	if maxvalue is not None:
 		enfit = np.clip(enfit, -float(maxvalue), float(maxvalue))
 
-	# Write the "QM" torsional energy into the .csv file
-	fout = open('qm_scan.csv', 'w')
-
-	fout.write(',Dihedral (rad),U_tors-qm - U_tors_qm[min] (kcal/mol),E_qm - E_qm[min] (kcal/mol),Non-bonded - Non-bonded[qm_min] (kcal/mol)\n')
-	for i in range(len(died)):
-			fout.write(str(i+1) + ',' + str(died[i]) + ',' + str(enfit[i]) + ',' + str(enqm[i]-min(enqm)) + ',' + str(nben[i]) + '\n')
+	return scan_fit, scan
 
 def write_itp_file(topfile: str, lr_data: dict):
 	"""
@@ -386,51 +475,77 @@ def write_itp_file(topfile: str, lr_data: dict):
 			fout.write(line)
 			line = f.readline()
 
-def linear_regression(topfile: str, method: str, lasso_alpha: float, weight_minimums: float):
+def linear_regression(scan: str, scan_fit: str, dih: str, dih_fit: str, topfile: str, method: str, lasso_alpha: float, weight_minimums: float, fit_from_total: bool) -> None:
 	"""
 	Perform the linear regression, generate a plot with classical and "quantum" torsional energies
 	and change the C_i coefficients in the topology file.
 
 	PARAMETERS:
+	scan [type: pd.DataFrame] - data from classical and quantum scan simulations
+	scan_fit [type: pd.DataFrame] - data from classical and quantum scan simulations to be fitted
+	dih [type: pd.DataFrame] - dihedral angles which changed during the scan
+	dih_fit [type: pd.DataFrame] - dihedral angles which changed during the scan and will be used for fitting
 	topfile [type: str] - topology (.itp) file
 	method [type: str] - the method used in the linear regression (least-square, ridge or lasso)
 	weight_minima [type: list] - the weights atributted to the total energy minimum points
+	fit_from_total [type: bool] - fit the dihedral from total energy
 
 	OUTPUT:
 	The "linear_regression.png" and "LR_*.itp" files.
 	"""
 
-	ans = pd.read_csv("qm_scan.csv", index_col=0)
-	data = pd.read_csv("dihedrals.csv", index_col=0)
+	if fit_from_total:
+		print("Not implemented yet.")
+		exit()
 
-	# Cubic polynomial interpolation
-	interpolarizer = CubicSpline(ans.iloc[:,0], ans.iloc[:,1])
+	else:
+		# Cubic polynomial interpolation
+		interpolarizer = CubicSpline(scan_fit.loc[:, 'Dihedral (rad)'], scan_fit.loc[:, 'U_tors-qm (kcal/mol)'])
 
 	# Add the interpolation in the dataFrame data
-	data["ans"] = data.apply(lambda x: interpolarizer(x[data.columns[0]]), axis=1)
+	dih_fit = dih_fit.copy()
+	dih_fit.loc[:, "ans"] = dih_fit.apply(lambda x: interpolarizer(x['Dihedral (rad)']), axis=1)
 
 	# Create a new data frame with ans values
-	df = data[["ans"]].copy().rename(columns={"ans":"y"})
+	df = dih_fit[["ans"]].copy().rename(columns={"ans":"y"})
 
 	# Compute each Ryckaert-Bellemans torsional element
-	for c in data.drop(columns=["ans"]).columns:
-		df[f"{c}_C0"] = np.cos(data[c])**0
-		df[f"{c}_C1"] = np.cos(data[c])**1
-		df[f"{c}_C2"] = np.cos(data[c])**2
-		df[f"{c}_C3"] = np.cos(data[c])**3
-		df[f"{c}_C4"] = np.cos(data[c])**4
-		df[f"{c}_C5"] = np.cos(data[c])**5
+	for c in dih_fit.drop(columns=["ans", "Dihedral (rad)"]).columns:
+		df[f"{c}_C0"] = np.cos(dih_fit[c])**0
+		df[f"{c}_C1"] = np.cos(dih_fit[c])**1
+		df[f"{c}_C2"] = np.cos(dih_fit[c])**2
+		df[f"{c}_C3"] = np.cos(dih_fit[c])**3
+		df[f"{c}_C4"] = np.cos(dih_fit[c])**4
+		df[f"{c}_C5"] = np.cos(dih_fit[c])**5
 
 	X = df.drop(columns=["y"])
 	y = df["y"]
 
 	# Apply the weight for total energy minimum points
-	weights = np.ones(len(ans))
+	weights = np.array([])
+	
+	for i in scan_fit.index.tolist():
+		if i > 0 and i < len(scan)-1:
+			if (scan.loc[i, 'E_qm - enqm_0 (kcal/mol)'] < scan.loc[i-1, 'E_qm - enqm_0 (kcal/mol)'] and 
+				scan.loc[i, 'E_qm - enqm_0 (kcal/mol)'] < scan.loc[i+1, 'E_qm - enqm_0 (kcal/mol)']):
+				weights = np.append(weights, weight_minimums)
+			else:
+				weights = np.append(weights, 1)
+		else:
+			weights = np.append(weights, 1)
 
-	for i in range(len(ans)):
-		if i > 0 and i < len(ans)-1:
-			if ans.iloc[i, 2] < ans.iloc[i-1, 2] and ans.iloc[i, 2] < ans.iloc[i+1, 2]:
-				weights[i] = weight_minimums
+	# Apply weights for minimal points among the chosen ones
+	# scan_fit = scan_fit.reset_index(drop=True)
+
+	# for i in scan_fit.index.tolist():
+	# 	if i > 0 and i < len(scan)-1:
+	# 		if (scan_fit.loc[i, 'E_qm - enqm_0 (kcal/mol)'] < scan_fit.loc[i-1, 'E_qm - enqm_0 (kcal/mol)'] and 
+	# 			scan_fit.loc[i, 'E_qm - enqm_0 (kcal/mol)'] < scan_fit.loc[i+1, 'E_qm - enqm_0 (kcal/mol)']):
+	# 			weights = np.append(weights, weight_minimums)
+	# 		else:
+	# 			weights = np.append(weights, 1)
+	# 	else:
+	# 		weights = np.append(weights, 1)
 
 	# Apply the linear regression model
 	if method == 'least-square':		
@@ -442,7 +557,7 @@ def linear_regression(topfile: str, method: str, lasso_alpha: float, weight_mini
 	elif method == 'lassocv':
 		reg = LassoCV(eps=1e-5, max_iter=10000000, fit_intercept=False)
 	else:
-		print("Either the method is not declared or is not implemented, using the default least square method.")
+		print("The method is not specified or not implemented, using the default least square method.")
 		reg = LinearRegression(fit_intercept=False)
 	
 	reg.fit(X, y, sample_weight=weights)
@@ -462,36 +577,58 @@ def linear_regression(topfile: str, method: str, lasso_alpha: float, weight_mini
 		for j in range(6):
 			lr_data[i]["constants"].append(reg.coef_[6*i+j])
 
+	# Write the linear regression coefficients in the topology file
+	write_itp_file(topfile, lr_data)
+
 	# Plot the results
-	x_plot = data[data.columns[0]].copy()
+
+	# Fitted dihedral energy
 	y_plot = np.dot(X.values, reg.coef_)
 
-	x_plot2 = np.linspace(x_plot.min(), x_plot.max(), 300)
-	smooth_dih = interp1d(x_plot, y_plot, kind='cubic')
-	smooth_nben = interp1d(x_plot, ans[ans.columns[3]], kind='cubic')
-	smooth_qmE = interp1d(x_plot, ans[ans.columns[2]], kind='cubic')
-	smooth_qmDih = interp1d(x_plot, ans[ans.columns[1]], kind='cubic')
+	# Dihedral angles
+	x_fit = scan_fit['Dihedral (rad)'].copy()
+	x_full = scan['Dihedral (rad)'].copy()
+
+	# X-axis points
+	x_plot1 = np.linspace(x_fit.min(), x_fit.max(), 300)
+	x_plot2 = np.linspace(x_full.min(), x_full.max(), 300)
+
+	# Interpolate required angles
+	smooth_dih = interp1d(x_fit, y_plot, kind='cubic')
+	smooth_nben_fit = interp1d(x_fit, scan_fit['Non-bonded - nben0 (kcal/mol)'].copy(), kind='cubic')
+
+	# Interpolate all angles available (npoints)
+	smooth_nben = interp1d(x_full, scan['Non-bonded - nben0 (kcal/mol)'].copy(), kind='cubic')
+	smooth_qmE = interp1d(x_full, scan['E_qm - enqm_0 (kcal/mol)'].copy(), kind='cubic')
+	smooth_qmDih = interp1d(x_full, scan['U_tors-qm (kcal/mol)'].copy(), kind='cubic')
 
 	fig, ((ax1), (ax2)) = plt.subplots(ncols=1, nrows=2, figsize=(7, 5))
 
-	ax1.plot(x_plot2, smooth_qmE(x_plot2), '--', c="tab:purple", label="Gaussian total energy")
-	ax1.plot(x_plot2, smooth_dih(x_plot2) + smooth_nben(x_plot2), '--', c="tab:green", label="Classical total energy")
-	ax1.scatter(ans[ans.columns[0]], ans[ans.columns[2]], c="tab:purple")
-	ax1.scatter(x_plot, y_plot + ans[ans.columns[3]], c="tab:green")
-	ax1.legend(frameon=False)
+	# Total energy
+	ax1.plot(x_plot2*180/np.pi, smooth_qmE(x_plot2), '--', c="tab:purple", label="Gaussian total energy")
+	ax1.scatter(x_full*180/np.pi, scan['E_qm - enqm_0 (kcal/mol)'].copy(), c="tab:purple")
 
-	ax2.plot(x_plot2, smooth_qmDih(x_plot2), '--', c="tab:red", label="Gaussian torsional energy")
-	ax2.plot(x_plot2, smooth_dih(x_plot2), '--', c="tab:orange", label="Fitted torsional energy")
-	ax2.scatter(ans[ans.columns[0]], ans[ans.columns[1]], c="tab:red")
-	ax2.scatter(x_plot, y_plot, c="tab:orange")
-	ax2.set_xlabel('Angle (rad)')
+	ax1.plot(x_plot1*180/np.pi, smooth_dih(x_plot1) + smooth_nben_fit(x_plot1), '--', c="tab:green", label="Classical total energy")
+	ax1.scatter(x_fit*180/np.pi, scan_fit['E_qm - enqm_0 (kcal/mol)'].copy(), c="tab:green", marker='x', s=50)
+
+	# Dihedral energy
+	ax2.plot(x_plot2*180/np.pi, smooth_qmDih(x_plot2), '--', c="tab:red", label="Gaussian torsional energy")
+	ax2.scatter(x_full*180/np.pi, scan['U_tors-qm (kcal/mol)'].copy(), c="tab:red")
+
+	ax2.plot(x_plot1*180/np.pi, smooth_dih(x_plot1), '--', c="tab:orange", label="Fitted torsional energy")
+	ax2.scatter(x_fit*180/np.pi, y_plot, c="tab:orange", marker='x', s=50)
+
+	# Plot setup
+	ax1.set_ylabel('Energy (kcal/mol)')
+	ax1.legend(frameon=False)
+	# ax1.set_ylim(-0.3, 2.3)
+
+	ax2.set_xlabel(r'Angle ($^o$)')
+	ax2.set_ylabel('Energy (kcal/mol)')
 	ax2.legend(frameon=False)
 
 	plt.tight_layout()
 	plt.savefig("linear_regression.png", bbox_inches='tight', format='png', dpi=600)
-
-	# Write the linear regression coefficients in the topology file
-	write_itp_file(topfile, lr_data)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Performs a linear regression to determine torsional dihedral coefficients.")
@@ -505,22 +642,25 @@ if __name__ == '__main__':
 	parser.add_argument("a3", type=int, help="third atom defining the reference dihedral.")
 	parser.add_argument("a4", type=int, help="fourth atom defining the reference dihedral.")
 	parser.add_argument("npoints", type=int, help="number of configurations during the scan.")
-	parser.add_argument("--method", "-m", help="the method employed in the linear regression (least-square, ridge, lasso).", 
+	parser.add_argument("--method", "-m", help="the method employed in the linear regression (least-square, ridge, lasso, lassocv).", 
 						default='least-square')
 	parser.add_argument("--alpha", type=float, help="the coefficient multiplying L1 penalty in Lasso linear regression (default = 0.01).", default=0.01)
 	parser.add_argument("--weight", "-w", type=float, help="the weight given to total energy minima points (default = 1).", default=1)
 	parser.add_argument("--cutoff", "-c", type=float, help="minimum atomic distance tolerated (default = 0.5 Angs).", default=0.5)
 	parser.add_argument("--remove-overlap", "-r", help="remove the overlapping configurations.", action='store_true')
 	parser.add_argument("--max-barrier", "-b", help="limit the torsional barriers to the provided value (in kcal/mol).", default=None)
-	parser.add_argument("--fit-from-total", "-t", help="fit the torsional angle using the total energy.", action='store_true')
+	parser.add_argument("--fit-from-total", "-t", help="fit the torsional angle using the total energy.", action='store_true', default=False)
+	parser.add_argument("--angles", nargs='+', type=float, help="all dihedrals (in degrees) to be used in the fit (include the max and min angles).")
 
 	args = parser.parse_args()
 
-	write_dih_csv(args.gaussianlogfile, args.txtfile, args.dfrfile, args.a1, args.a2, args.a3, args.a4, args.max_barrier)
+	data = get_data(args.gaussianlogfile, args.txtfile, args.dfrfile, args.a1, args.a2, args.a3, args.a4)
 
-	write_torsional_changes(args.xyzrotationsfile, args.topfile, args.a1, args.a2, args.a3, args.a4, args.npoints)
+	scan_fit, scan = rescale_data(data, args.angles, args.max_barrier)
+
+	dih_fit, dih = get_dihedrals(args.xyzrotationsfile, args.topfile, args.a1, args.a2, args.a3, args.a4, args.npoints, args.angles)
 
 	if args.remove_overlap:
 		remove_overlap(args.topfile, args.xyzrotationsfile, args.cutoff, args.npoints)
 
-	linear_regression(args.topfile, args.method, args.alpha, args.weight)
+	linear_regression(scan, scan_fit, dih, dih_fit, args.topfile, args.method, args.alpha, args.weight, args.fit_from_total)
